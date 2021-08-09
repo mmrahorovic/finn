5fa0e01fc5f0cd88e5849216f53d866c0e8620ab
mmrahorovic <mmrahorovic@hotmail.com>
2021-08-09 18:50:33 +0200

    test scripts for analyzing StreamingFCLayer_Batch and VVAU

---
+++ tests/end2end/test_streamingfc_ooc.py
@@ -0,0 +1,39 @@
+import os
+
+from finn.core.modelwrapper import ModelWrapper
+from finn.transformation.general import ApplyConfig
+from finn.transformation.fpgadataflow.prepare_ip import PrepareIP
+from finn.transformation.fpgadataflow.hlssynth_ip import HLSSynthIP
+from finn.transformation.fpgadataflow.create_stitched_ip import CreateStitchedIP
+from finn.transformation.fpgadataflow.synth_ooc import SynthOutOfContext
+from finn.util.basic import alveo_part_map, alveo_default_platform
+
+build_dir = os.environ["FINN_BUILD_DIR"]
+test_board = "U250"
+test_platform = alveo_default_platform[test_board]
+test_fpga_part = alveo_part_map[test_board]
+target_clk_ns = 6
+
+def run_ooc():
+    model = ModelWrapper(build_dir+"/fclayer_0.onnx")
+
+    print("Running ApplyConfig...")
+    model = model.transform(ApplyConfig(build_dir+"/fold_config.json"))
+    model.save(build_dir+"/streamingfclayer0_config.onnx")
+
+    print("Running PrepareIP...")
+    model = model.transform(PrepareIP(test_fpga_part, target_clk_ns))
+    model.save(build_dir+"/streamingfclayer0_prepareip.onnx")
+
+    print("Running HLSSynthIP...")
+    model = model.transform(HLSSynthIP())
+    model.save(build_dir+"/streamingfclayer0_hlssynthip.onnx")
+
+    print("Running CreateStitchedIP...")
+    model = model.transform(CreateStitchedIP(test_fpga_part, target_clk_ns))
+    model.save(build_dir+"/streamingfclayer0_stitchedip.onnx")
+
+    print("Running SynthOutOfContext...")
+    model = model.transform(SynthOutOfContext(test_fpga_part, target_clk_ns))
+    model.save(build_dir+"/streamingfclayer0_synthooc.onnx")
+
